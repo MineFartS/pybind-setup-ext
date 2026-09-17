d6c747461ef013eb7afe299df96ed688ee0ddb76
Phil Hunt <philiphuntiv2@gmail.com>
2026-09-17 09:24:14 -0400

    cpp_ext - specify platforms

--- pybind_setup_ext/__init__.py
+++ pybind_setup_ext/__init__.py
@@ -9,6 +9,6 @@ __all__ = ["cpp_ext", "setup", "update_submodule"]
 def setup(*ext_modules:cpp_ext) -> None:
     st.setup(
         cmdclass = {'build_ext': build_ext},
-        ext_modules = list(ext_modules),
+        ext_modules = list(filter(None, ext_modules)),
     )
 
--- pybind_setup_ext/cpp_ext.py
+++ pybind_setup_ext/cpp_ext.py
@@ -1,9 +1,12 @@
 from pybind11 import get_include
+from typing import Literal
 from copy import deepcopy
 from sys import platform
 from pathlib import Path
 import setuptools as st
 from glob import glob
+
+_plats = Literal['win32', 'linux', 'darwin']
 
 _kw_templ = {
 
@@ -24,10 +27,14 @@ class cpp_ext(st.Extension):
 
     def __init__(self,
         cpp_path: str|Path,
+        *,
+        platforms: list[_plats] = ['win32', 'linux', 'darwin'],
         extra_objects: str = None,
         include_dirs: list[str] = [],
         sources: list[str] = [],
     ) -> None:
+
+        self.platforms = platforms
 
         if not isinstance(cpp_path, Path):
             cpp_path = Path(cpp_path)
@@ -50,4 +57,7 @@ class cpp_ext(st.Extension):
             _kw['extra_objects'] = glob(extra_objects)
 
         super().__init__(**_kw)
+
+    def __bool__(self) -> bool:
+        return platform in self.platforms
 
