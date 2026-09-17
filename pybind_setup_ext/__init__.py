3a2257e61f41e91587365055cb4ab1eaa574b9cd
Phil Hunt <philiphuntiv2@gmail.com>
2026-09-17 08:57:11 -0400

    Create __init__.py

---
+++ pybind_setup_ext/__init__.py
@@ -0,0 +1,13 @@
+
+from .build_ext import build_ext
+from .cpp_ext import cpp_ext
+import setuptools as st
+
+__all__ = ["cpp_ext", "setup"]
+
+def setup(*ext_modules:cpp_ext) -> None:
+    st.setup(
+        cmdclass = {'build_ext': build_ext},
+        ext_modules = list(ext_modules),
+    )
+
